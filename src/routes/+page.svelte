8bb33970ba3b99f9407cf04423b32cb2ff300310
dehadeaaryan <dehadeaaryan@gmail.com>
2026-05-11 11:29:50 -0700

    update seo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,23 @@
 	import Projects from '$lib/components/Projects.svelte';
 </script>
 
+<svelte:head>
+	<title>Aaryan Dehade | Software Engineer</title>
+	<meta name="description" content="Aaryan Dehade's Portfolio Website." />
+
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://aaryandehade.com/" />
+	<meta property="og:title" content="Aaryan Dehade | Software Engineer" />
+	<meta property="og:description" content="Aaryan Dehade's Portfolio Website." />
+	<meta property="og:image" content="https://aaryandehade.com/apple-touch-icon.png" />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:url" content="https://aaryandehade.com/" />
+	<meta name="twitter:title" content="Aaryan Dehade | Software Engineer" />
+	<meta name="twitter:description" content="Aaryan Dehade's Portfolio Website." />
+	<meta name="twitter:image" content="https://aaryandehade.com/apple-touch-icon.png" />
+</svelte:head>
+
 <Header />
 <main class="relative">
 	<Landing />
