38715186ac87a2dde077d5046ed996c63d8f5025
dehadeaaryan <dehadeaaryan@gmail.com>
2026-07-28 01:22:30 -0700

    Configure Better Auth trustedOrigins and secure cookies for HTTPS proxy

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -6,10 +6,18 @@ import { getRequestEvent } from '$app/server';
 import { db } from '$lib/server/db';
 
 export const auth = betterAuth({
-	baseURL: env.ORIGIN,
+	baseURL: env.ORIGIN || 'https://aaryandehade.com',
 	secret: env.BETTER_AUTH_SECRET,
 	database: drizzleAdapter(db, { provider: 'pg' }),
-	emailAndPassword: { enabled: true },
+	trustedOrigins: [
+		'https://aaryandehade.com',
+		'https://www.aaryandehade.com',
+		'http://localhost:5173',
+		'http://localhost:3000'
+	],
+	advanced: {
+		useSecureCookies: true
+	},
 	socialProviders: {
 		google: {
 			clientId: env.GOOGLE_CLIENT_ID || '',
@@ -20,4 +28,3 @@ export const auth = betterAuth({
 		sveltekitCookies(getRequestEvent) // make sure this is the last plugin in the array
 	]
 });
-
